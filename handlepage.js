de7eb665eeb2d4cbf48b7a9571bb89a25da741ce
irutori <irutack@gmail.com>
2013-01-06 20:11:10 +0900

    cahnge ContentsScript

--- handlepage.js
+++ handlepage.js
@@ -3,3 +3,21 @@
 function hoge(){
 	alert("");
 }
+
+main();
+
+function main(){
+	count= 0;
+	//setInterval("submitclick()",1000);
+}
+/*a [href*='Fevent_date']*/
+function submitclick() {
+	if(count == 3){
+		$("a[href*='Fevent_date']")[0].click();
+		count++;
+	}else if(count<3){
+		count++;
+	}else{
+		return;
+	}
+}
